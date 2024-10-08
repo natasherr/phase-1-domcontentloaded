a0a73458ff508cbe4338cf65e036774baf75a5c6
Ashley Natasha <ashleyotsiula@gmail.com>
2024-10-08 16:06:34 +0300

    domloaded

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const changeP = document.getElementById("text")
+    changeP.innerHTML = "This is really cool!"
+})
